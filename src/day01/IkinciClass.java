a3d389469d05d3ff65424909d2ead2042c6607c3
Mehmet <myesil3445@gmail.com>
2020-08-09 21:02:09 +0200

    ilk commitim

---
+++ src/day01/IkinciClass.java
@@ -0,0 +1,10 @@
+package day01;
+
+public class IkinciClass {
+
+	public static void main(String[] args) {
+		System.out.println("selamlar");
+
+	}
+
+}
---
+++ src/day01/IlkClass.java
@@ -0,0 +1,11 @@
+package day01;
+
+public class IlkClass {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Mehmet'den selamlar");
+
+	}
+
+}
